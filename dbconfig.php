33166d61fc33f3bbba6f65b143d47d2a1e014fb4
LigorioSalgado <salgadolg5@hotmail.com>
2015-04-11 12:01:13 -0500

    Elementos del servidor completos

--- dbconfig.php
+++ dbconfig.php
@@ -1,8 +1,9 @@
 <?php
-define('DB_SERVER', 'localhost');
-define('DB_USERNAME', 'username');    // DB username
-define('DB_PASSWORD', 'password');    // DB password
-define('DB_DATABASE', 'database');      // DB name
-$connection = mysql_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD) or die( "Unable to connect");
-$database = mysql_select_db(DB_DATABASE) or die( "Unable to select database");
+define("DB_SERVER", "local.host.com");
+define("DB_USERNAME", "root");    // DB username
+define("DB_PSSWORD", "123456");    // DB password
+define("DB_DATABASE", "fhb");      // DB name
+//$connection = mysql_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD) or die( "Unable to connect");
+$link = mysqli_connect("local.host.com", "root", "123456","fhb") or die("Error " . mysqli_error($link));
+//$database = mysqli_select_db(DB_DATABASE) or die( "Unable to select database");
 ?>
--- fbconfig.php
+++ fbconfig.php
@@ -2,6 +2,7 @@
 session_start();
 // added in v4.0.0
 require_once 'autoload.php';
+require_once 'dbconfig.php';
 use Facebook\FacebookSession;
 use Facebook\FacebookRedirectLoginHelper;
 use Facebook\FacebookRequest;
@@ -13,10 +14,13 @@ use Facebook\GraphObject;
 use Facebook\Entities\AccessToken;
 use Facebook\HttpClients\FacebookCurlHttpClient;
 use Facebook\HttpClients\FacebookHttpable;
+
+$hash =  rand(1000,9999);
+
 // init app with app id and secret
 FacebookSession::setDefaultApplication( '341311466063931','50b25d09ed5241a5dd20530bdbae1156' );
 // login helper with redirect_uri
-    $helper = new FacebookRedirectLoginHelper('https://quiet-badlands-1419.herokuapp.com/fbconfig.php' );
+    $helper = new FacebookRedirectLoginHelper('http://local.host.com/FHB/fbconfig.php' );
 try {
   $session = $helper->getSessionFromRedirect();
 } catch( FacebookRequestException $ex ) {
@@ -38,10 +42,28 @@ if ( isset( $session ) ) {
 	    $_SESSION['FBID'] = $fbid;
         $_SESSION['FULLNAME'] = $fbfullname;
 	    $_SESSION['EMAIL'] =  $femail;
+      checkuser($fbid,$fbfullname,$femail,$hash);
+
     /* ---- header location after session ----*/
   header("Location: index.php");
 } else {
   $loginUrl = $helper->getLoginUrl();
  header("Location: ".$loginUrl);
 }
+
+
+function checkuser($fuid,$ffname,$femail,$h){
+      $check = mysqli_query($link,"select * from users where FBID='$fuid'");
+    if (empty($check)) { // if new user . Insert a new record
+  $query = "INSERT INTO users (FBID,Nombre,Correo,clavecel) VALUES ('$fuid','$ffname','$femail','$h')";
+  mysqli_query($link,$query);
+  } else {   // If Returned user . update the user record
+  $query = "UPDATE users SET Nombre='$ffname', Correo='$femail' where FBID='$fuid'";
+  mysqli_query($link, $query);
+  }
+
+}
+
+
+
 ?>
