9c8e0b9b39db3c7656a42be281b32a157efb6b22
LigorioSalgado <salgadolg5@hotmail.com>
2015-04-11 05:41:57 -0500

    Tercera Prueba

--- config/facebook.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-$config = array(
-	'app_id' => '341311466063931',
-	'app_secret' => '50b25d09ed5241a5dd20530bdbae1156',
-	'scopes' => array('email','read_friendlists','user_online_presence')
-);
